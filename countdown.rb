887d136af7298bae7b20e69dae45f52f60ac7ad1
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-05 23:14:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@ def countdown(countdown_output)
 countdown_output = 10
 while countdown_output > 10
   puts "#{countdown_output} SECOND(S)!"
-  countdown_output - 1
+  countdown_output -= 1
 
 end
 end
